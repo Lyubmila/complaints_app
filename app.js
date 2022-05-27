d169a55744e7e8ab63ec4110974f4a10663e1174
unknown <jasonmui2004@gmail.com>
2022-05-27 15:03:26 -0400

    new changes to css and js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
       let choices = document.querySelectorAll('.choice')
       
       function findBorough (e) {  
-          
+       clearScreen();   
       let limit = Number(document.getElementById('comps').value);
        
        if (limit == '')   limit = 10
@@ -11,14 +11,17 @@
          
          fetch('https://data.cityofnewyork.us/resource/erm2-nwe9.json?borough=BROOKLYN&agency=NYPD&$limit='+ limit).then(response => response.json()).then(users => { 
             
-            let div = "";
+         
             users.forEach ((item) => {                                            
                
-               div +=  "<div>" + item.complaint_type + "</div>"
-               div += "<button class=show>"+ `What did the Police Do?` + "</button>"
-               div += "<div class=hidden>" + item.resolution_description + "</div>"                         
-      } ) 
-               document.getElementById("container").innerHTML = div;            
+               const parentDiv = document.querySelector('#container')
+               const newDiv= document.createElement("div");
+               newDiv.innerHTML= `<div class=item><div>${item.complaint_type}</div> <div><button class=show> What did the Police Do? </button> <div class=hidden>${item.resolution_description}</div></div>`
+               parentDiv.appendChild(newDiv)
+
+                
+         } ) 
+                         
          
          }).catch(err => console.log(err))
             
@@ -26,30 +29,31 @@
       }  else if (e.target.id === 'bronx') {
          fetch('https://data.cityofnewyork.us/resource/erm2-nwe9.json?borough=BRONX&agency=NYPD&$limit='+ limit).then(response => response.json()).then(users => { 
             
-            let div = "";
+            
             users.forEach ((item) => {                                            
-               
-               div +=  "<div>" + item.complaint_type + "</div>"
-               div += `<button class=show>`+ `What did the Police Do?` + "</button>"
-               div += "<div class=hidden>" + item.resolution_description + "</div>"                         
+                
+               const parentDiv = document.querySelector('#container')
+               const newDiv= document.createElement("div");
+               newDiv.innerHTML= `<div class=item><div>${item.complaint_type}</div> <div><button class=show> What did the Police Do? </button> <div class=hidden>${item.resolution_description}</div></div>`
+               parentDiv.appendChild(newDiv)                       
                
             } ) 
-               document.getElementById("container").innerHTML = div;
+                
             
          
          }).catch(err => console.log(err))
       }  else if (e.target.id === 'manhattan') { 
          fetch('https://data.cityofnewyork.us/resource/erm2-nwe9.json?borough=MANHATTAN&agency=NYPD&$limit='+ limit).then(response => response.json()).then(users => { 
             
-            let div = "";
+           
             users.forEach ((item) => {                                            
-               
-               div +=  "<div>" + item.complaint_type + "</div>"
-               div += `<button class=show>`+ `What did the Police Do?` + "</button>"
-               div += "<div class=hidden>" + item.resolution_description + "</div>"                         
+             
+               const parentDiv = document.querySelector('#container')
+               const newDiv= document.createElement("div");
+               newDiv.innerHTML= `<div class=item><div>${item.complaint_type}</div> <div><button class=show> What did the Police Do? </button> <div class=hidden>${item.resolution_description}</div></div>`
+               parentDiv.appendChild(newDiv)                         
                
             } ) 
-               document.getElementById("container").innerHTML = div;
             
          
          }).catch(err => console.log(err))
@@ -58,13 +62,14 @@
       }  else if (e.target.id === 'queens') { 
          fetch('https://data.cityofnewyork.us/resource/erm2-nwe9.json?borough=QUEENS&agency=NYPD&$limit='+ limit).then(response => response.json()).then(users => { 
             
-            let div = "";
+            
             users.forEach ((item) => {                                            
-               
-               div +=  "<div>" + item.complaint_type + "</div>"
-               div += `<button class=show>`+ `What did the Police Do?` + "</button>"
-               div += "<div class=hidden>" + item.resolution_description + "</div>"                         
-               
+              
+               const parentDiv = document.querySelector('#container')
+               const newDiv= document.createElement("div");
+               newDiv.innerHTML= `<div class=item><div>${item.complaint_type}</div> <div><button class=show> What did the Police Do? </button> <div class=hidden>${item.resolution_description}</div></div>`
+               parentDiv.appendChild(newDiv)
+            
             } ) 
                document.getElementById("container").innerHTML = div;
             
@@ -73,15 +78,16 @@
       }  else if (e.target.id === 'staten_island') { 
          fetch('https://data.cityofnewyork.us/resource/erm2-nwe9.json?borough=STATEN ISLAND&agency=NYPD&$limit='+ limit).then(response => response.json()).then(users => { 
             
-            let div = "";
+            
             users.forEach ((item) => {                                            
                
-               div +=  "<div>" + item.complaint_type + "</div>"
-               div += `<button class=show >`+ `What did the Police Do?` + "</button>"
-               div += "<div class=hidden >" + item.resolution_description + "</div>"                         
+            const parentDiv = document.querySelector('#container')
+            const newDiv= document.createElement("div");
+            newDiv.innerHTML= `<div class=item><div>${item.complaint_type}</div> <div><button class=show> What did the Police Do? </button> <div class=hidden>${item.resolution_description}</div></div>`
+            parentDiv.appendChild(newDiv)                
                
             } ) 
-               document.getElementById("container").innerHTML = div;
+               
             
          
          }).catch(err => console.log(err))
@@ -91,6 +97,11 @@
       document.getElementById('comps').value = ''  //reset input to 0
 
     } // end of function
- 
+
+    function clearScreen () {
+       const div =document.getElementById('container')
+       const divs = div.querySelectorAll('div')
+       divs.forEach(child => child.remove() )
+    }
 
 choices.forEach(choice => choice.addEventListener('click', findBorough))
