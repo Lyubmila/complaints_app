565a9fc59f2429606b297b85f563f66bc14240dd
Lyubmila <Lyubmila@gmail.com>
2022-05-29 16:39:33 -0400

    new updates from Jason

--- app.js
+++ app.js
@@ -1,96 +1,322 @@
-      console.log("hello")
+console.log("hello")
       let choices = document.querySelectorAll('.choice')
       
-      function findBorough (e) {  
-          
-      let limit = Number(document.getElementById('comps').value);
+      function findBorough (e) {  // find complaints by borough
+       clearScreen();   // remove all elements after each button push
+       let limit = Number(document.getElementById('comps').value);
        
        if (limit == '')   limit = 10
-         
+       
+       
+      
        if (e.target.id === 'brooklyn'){
-         
+         let itemCount = []
+        // fetch data according to borough, agemcy=NYPD, order by complaint type, limit=number of complaints to show      
          fetch('https://data.cityofnewyork.us/resource/erm2-nwe9.json?borough=BROOKLYN&agency=NYPD&$limit='+ limit).then(response => response.json()).then(users => { 
             
-            let div = "";
             users.forEach ((item) => {                                            
+              // create the complaint elements
+               const parentDiv = document.querySelector('#container')
+               const newDiv= document.createElement("div");
+               newDiv.className = "complaints"
+               parentDiv.appendChild(newDiv)
+               const divKid = document.createElement('div')
+               divKid.className = 'type'
+               divKid.textContent = item.descriptor
+               newDiv.appendChild(divKid)
+               const button = document.createElement('button')
+               button.className='show'
+               button.textContent="What did the Police Do?"
+               newDiv.appendChild(button)
+               const div3 = document.createElement('div')
+               div3.className='hidden'
+               div3.textContent = item.resolution_description
+               newDiv.appendChild(div3)               
+               itemCount.push( item.descriptor)
+                 
+            
+              } ) //end users foreach
+            
+            const freq = getFrequency(itemCount)             
                
-               div +=  "<div>" + item.complaint_type + "</div>"
-               div += "<button class=show>"+ `What did the Police Do?` + "</button>"
-               div += "<div class=hidden>" + item.resolution_description + "</div>"                         
-      } ) 
-               document.getElementById("container").innerHTML = div;            
+             // Show frequency of complaints
+             const complaintTimes = document.getElementById('frequency')
+             let message = ''
+             for (const property in freq){
+                message += `${property} was complained about  ${freq[property]} times.<br>`
+              }
+              
+             complaintTimes.innerHTML = message;
+
+            
+           
+            // add event listeners to each button
+            const resolutions = document.querySelectorAll('.show')
+            resolutions.forEach( reso =>{
+                  reso.addEventListener("click",function(event) {  
+                     let content = event.target.nextElementSibling; // target the element under the button 
+                     if (content.style.display === "block") {
+                     content.style.display = "none";
+                     } else {
+                     content.style.display = "block";
+                     }
+
+                  })
+            })
+                
+            
          
          }).catch(err => console.log(err))
             
                            
-      }  else if (e.target.id === 'bronx') {
+       }  else if (e.target.id === 'bronx') {
+          let itemCount = []
          fetch('https://data.cityofnewyork.us/resource/erm2-nwe9.json?borough=BRONX&agency=NYPD&$limit='+ limit).then(response => response.json()).then(users => { 
             
-            let div = "";
             users.forEach ((item) => {                                            
-               
-               div +=  "<div>" + item.complaint_type + "</div>"
-               div += `<button class=show>`+ `What did the Police Do?` + "</button>"
-               div += "<div class=hidden>" + item.resolution_description + "</div>"                         
-               
+              
+               const parentDiv = document.querySelector('#container')
+               const newDiv= document.createElement("div");
+               newDiv.className = "complaints"
+               parentDiv.appendChild(newDiv)
+               const divKid = document.createElement('div')
+               divKid.className = 'type'
+               divKid.textContent = item.descriptor
+               newDiv.appendChild(divKid)
+               const button = document.createElement('button')
+               button.className='show'
+               button.textContent="What did the Police Do?"
+               newDiv.appendChild(button)
+               const div3 = document.createElement('div')
+               div3.className='hidden'
+               div3.textContent = item.resolution_description
+               newDiv.appendChild(div3)               
+               itemCount.push( item.descriptor)
+         
+                            
             } ) 
-               document.getElementById("container").innerHTML = div;
+
+            const freq = getFrequency(itemCount)             
+               
+            // Show frequency of complaints
+            const complaintTimes = document.getElementById('frequency')
+            let message = ''
+            for (const property in freq){
+               message += `${property} was complained about  ${freq[property]} times.<br>`
+            }
+            
+            complaintTimes.innerHTML = message;
+            
+               // add event listeners to buttons
+            const resolutions = document.querySelectorAll('.show')
+            resolutions.forEach( reso =>{
+                  reso.addEventListener("click",function(event) {  
+                     let content = event.target.nextElementSibling; // target the next element 
+                     if (content.style.display === "block") {
+                     content.style.display = "none";
+                     } else {
+                     content.style.display = "block";
+                     }
+
+                  })
+            })
+                
             
          
          }).catch(err => console.log(err))
       }  else if (e.target.id === 'manhattan') { 
+          let itemCount = []
          fetch('https://data.cityofnewyork.us/resource/erm2-nwe9.json?borough=MANHATTAN&agency=NYPD&$limit='+ limit).then(response => response.json()).then(users => { 
             
-            let div = "";
+           
             users.forEach ((item) => {                                            
-               
-               div +=  "<div>" + item.complaint_type + "</div>"
-               div += `<button class=show>`+ `What did the Police Do?` + "</button>"
-               div += "<div class=hidden>" + item.resolution_description + "</div>"                         
-               
+              
+               const parentDiv = document.querySelector('#container')
+               const newDiv= document.createElement("div");
+               newDiv.className = "complaints"
+               parentDiv.appendChild(newDiv)
+               const divKid = document.createElement('div')
+               divKid.className = 'type'
+               divKid.textContent = item.descriptor
+               newDiv.appendChild(divKid)
+               const button = document.createElement('button')
+               button.className='show'
+               button.textContent="What did the Police Do?"
+               newDiv.appendChild(button)
+               const div3 = document.createElement('div')
+               div3.className='hidden'
+               div3.textContent = item.resolution_description
+               newDiv.appendChild(div3)               
+               itemCount.push(item.descriptor)
+         
+                            
             } ) 
-               document.getElementById("container").innerHTML = div;
+             
+            const freq = getFrequency(itemCount)             
+               
+            // Show frequency of complaints
+            const complaintTimes = document.getElementById('frequency')
+            let message = ''
+            for (const property in freq){
+               message += `${property} was complained about  ${freq[property]} times.<br>`
+             }
+             
+            complaintTimes.innerHTML = message;
+
+            // add event listeners
+            const resolutions = document.querySelectorAll('.show')
+            resolutions.forEach( reso =>{
+                  reso.addEventListener("click",function(event) {  
+                     let content = event.target.nextElementSibling; // target the next element 
+                     if (content.style.display === "block") {
+                     content.style.display = "none";
+                     } else {
+                     content.style.display = "block";
+                     }
+
+                  })
+            })
             
          
          }).catch(err => console.log(err))
            
 
       }  else if (e.target.id === 'queens') { 
+         let itemCount = [] 
          fetch('https://data.cityofnewyork.us/resource/erm2-nwe9.json?borough=QUEENS&agency=NYPD&$limit='+ limit).then(response => response.json()).then(users => { 
             
-            let div = "";
-            users.forEach ((item) => {                                            
-               
-               div +=  "<div>" + item.complaint_type + "</div>"
-               div += `<button class=show>`+ `What did the Police Do?` + "</button>"
-               div += "<div class=hidden>" + item.resolution_description + "</div>"                         
-               
-            } ) 
-               document.getElementById("container").innerHTML = div;
             
+            users.forEach ((item) => {                                            
+              
+               const parentDiv = document.querySelector('#container')
+               const newDiv= document.createElement("div");
+               newDiv.className = "complaints"
+               parentDiv.appendChild(newDiv)
+               const divKid = document.createElement('div')
+               divKid.className = 'type'
+               divKid.textContent = item.descriptor
+               newDiv.appendChild(divKid)
+               const button = document.createElement('button')
+               button.className='show'
+               button.textContent="What did the Police Do?"
+               newDiv.appendChild(button)
+               const div3 = document.createElement('div')
+               div3.className='hidden'
+               div3.textContent = item.resolution_description
+               newDiv.appendChild(div3)               
+               itemCount.push(item.descriptor)
+         
+                            
+            } ) 
+
+            const freq = getFrequency(itemCount)             
+               
+            // Show frequency of complaints
+            const complaintTimes = document.getElementById('frequency')
+            let message = ''
+            for (const property in freq){
+               message += `${property} was complained about  ${freq[property]} times.<br>`
+             }
+             
+            complaintTimes.innerHTML = message;
+
+            // add event listeners
+            const resolutions = document.querySelectorAll('.show')
+            resolutions.forEach( reso =>{
+                  reso.addEventListener("click",function(event) {  
+                     let content = event.target.nextElementSibling; // target the next element 
+                     if (content.style.display === "block") {
+                     content.style.display = "none";
+                     } else {
+                     content.style.display = "block";
+                     }
+
+                  })
+            })
+             
          
          }).catch(err => console.log(err))
       }  else if (e.target.id === 'staten_island') { 
+         let itemCount = []
          fetch('https://data.cityofnewyork.us/resource/erm2-nwe9.json?borough=STATEN ISLAND&agency=NYPD&$limit='+ limit).then(response => response.json()).then(users => { 
             
-            let div = "";
+            
+              
             users.forEach ((item) => {                                            
-               
-               div +=  "<div>" + item.complaint_type + "</div>"
-               div += `<button class=show >`+ `What did the Police Do?` + "</button>"
-               div += "<div class=hidden >" + item.resolution_description + "</div>"                         
-               
+              
+               const parentDiv = document.querySelector('#container')
+               const newDiv= document.createElement("div");
+               newDiv.className = "complaints"
+               parentDiv.appendChild(newDiv)
+               const divKid = document.createElement('div')
+               divKid.className = 'type'
+               divKid.textContent = item.descriptor
+               newDiv.appendChild(divKid)
+               const button = document.createElement('button')
+               button.className='show'
+               button.textContent="What did the Police Do?"
+               newDiv.appendChild(button)
+               const div3 = document.createElement('div')
+               div3.className='hidden'
+               div3.textContent = item.resolution_description
+               newDiv.appendChild(div3)               
+               itemCount.push(item.descriptor)
+         
+                            
             } ) 
-               document.getElementById("container").innerHTML = div;
+
+            const freq = getFrequency(itemCount)             
+               
+            // Show frequency of complaints
+            const complaintTimes = document.getElementById('frequency')
+            let message = ''
+            for (const property in freq){
+               message += `${property} was complained about  ${freq[property]} times.<br>`
+             }
+             
+            complaintTimes.innerHTML = message;
+
+            // add event listeners
+            const resolutions = document.querySelectorAll('.show')
+            resolutions.forEach( reso =>{
+                  reso.addEventListener("click",function(event) {  
+                     let content = event.target.nextElementSibling;
+                     if (content.style.display === "block") {
+                     content.style.display = "none";
+                     } else {
+                     content.style.display = "block";
+                     }
+
+                  })
+            })
+               
             
          
          }).catch(err => console.log(err))
       
      }      
 
-      document.getElementById('comps').value = ''  //reset input to 0
+      document.getElementById('comps').value = ''  //reset input to none
 
     } // end of function
- 
+
+      function clearScreen () { // kill the starks
+         const houseStark =document.getElementById('container')
+         const starkChildren = houseStark.querySelectorAll('div')
+         starkChildren.forEach(child => child.remove() )
+      } 
+
+      // return number of occurence of each complaint
+      function getFrequency (arr) {
+         const complaints = {};
+         arr.forEach(item => {
+            if(complaints[item]){
+               complaints[item]++;
+            }else{
+               complaints[item] = 1;
+            }
+         });
+         return complaints;
+      };
 
 choices.forEach(choice => choice.addEventListener('click', findBorough))
